0f048a4042655782c58cb73108d624c462251b37
JeongSeongGil <63443366+JeongSeongGil@users.noreply.github.com>
2021-03-12 19:00:37 +0900

    Add files via upload

---
+++ Ex03_04.java
@@ -0,0 +1,11 @@
+
+public class Ex03_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%d / %d = %.1f \n", 100, 200, 0.5); // 정수 2개와 실수 1개 , (.1f = 0.50000000 =>  0.5)
+		System.out.printf("%c %c \n", 'a', 'K'); // 문자 2개
+		System.out.printf("%s %s \n", "IT CookBook", "JAVA"); // 문자열 2개
+	}
+
+}
