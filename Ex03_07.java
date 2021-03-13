e8bdf6fef26e9e2f6fa0789aa1affd77ad9416e5
JeongSeongGil <63443366+JeongSeongGil@users.noreply.github.com>
2021-03-13 18:45:31 +0900

    Add files via upload

---
+++ Ex03_07.java
@@ -0,0 +1,16 @@
+
+public class Ex03_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		float b;
+		a = (int) 123.45f;          // (int) 123.45f ==> float값을 int로 변환
+		b = 200;
+		
+		System.out.printf("a ==> %d\n", a);
+		System.out.printf("b ==> %f", b);
+
+	}
+
+}
