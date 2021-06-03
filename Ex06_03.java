c54aa66ab8864024dff5c64b8469be39f28bd7dd
JeongSeongGil <63443366+JeongSeongGil@users.noreply.github.com>
2021-06-03 10:11:39 +0900

    Add files via upload

---
+++ Ex06_03.java
@@ -0,0 +1,18 @@
+
+public class Ex06_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for (i = 0; i < 3; i++) {
+			System.out.printf("안녕하세요?\n");
+			System.out.printf("%d # 또 안녕하세요? #\n", i);
+		}
+		System.out.println("\n");
+		for (i = 0; i < 3; i++) {
+			System.out.printf("안녕하세요?\n");
+			System.out.printf("%d ## 또 안녕하세요? ##\n", i);
+		}
+
+	}
+}
